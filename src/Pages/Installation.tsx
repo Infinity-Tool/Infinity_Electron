0b813c0badb8053f3c1c1196d7b99a1967355857
Alexander Trimble <trimble.a@proton.me>
2023-10-12 17:15:57 -0600

    misc

--- src/Pages/Installation.tsx
+++ src/Pages/Installation.tsx
@@ -30,7 +30,6 @@ export default function Installation() {
   const [step1Selection] = useLocalStorage(LocalStorageKeys.step1Selection, []);
   const [filesCompleted, setFilesCompleted]: any = useState([]);
   const [filesErrored, setFilesErrored]: any = useState([]);
-  const [done, setDone]: any = useState(false);
 
   const downloadProgress = useMemo(() => {
     const totalFiles = fileCount;
@@ -119,164 +118,9 @@ export default function Installation() {
     };
   }, [ipcRenderer]);
 
-  // useEffect(() => {
-  //   buildFileLists();
-  // }, [step1Selection]);
-
-  // useEffect(() => {
-  //   ipcRenderer.on("download-complete", (event: any, file: any) => {
-  //     console.log("[download-complete]", file);
-  //     onFileDone(event, file);
-  //   });
-
-  //   ipcRenderer.on("download-error", (event: any, file: any) => {
-  //     console.log("[download-error]", file);
-  //     onFileError(event, file);
-  //   });
-
-  //   return () => {
-  //     ipcRenderer.removeAllListeners("download-complete");
-  //     ipcRenderer.removeAllListeners("download-error");
-  //   };
-  // }, [ipcRenderer]);
-
-  // useEffect(() => {
-  //   downloadNextFile();
-  // }, [filesInQueue]);
-
-  // const downloadNextFile = () => {
-  //   if (filesInQueue.length > 0) {
-  //     const nextFile = getOneFromQueue();
-  //     if (nextFile) {
-  //       addToInProgressFiles(nextFile);
-  //       // removeFromFilesToDownload(nextFile);
-  //       const directoryWithoutFileName = nextFile.destination.substring(
-  //         0,
-  //         nextFile.destination.lastIndexOf("/")
-  //       );
-  //       // console.log("[downloadNextFile]", nextFile.fileName);
-  //       ipcRenderer.send("download-file", {
-  //         url: nextFile.source,
-  //         properties: {
-  //           directory: directoryWithoutFileName,
-  //           fileName: nextFile.fileName,
-  //           overwrite: true,
-  //         },
-  //       });
-  //     } else {
-  //       setDone(true);
-  //     }
-  //   }
-  // };
-
-  // //Functions
-  // const buildFileLists = () => {
-  //   let modFiles: any = [];
-
-  //   step1Selection.forEach((selected: any) => {
-  //     const foundEntry = availableFiles.find(
-  //       (available: any) => selected.name === available.name
-  //     );
-
-  //     if (foundEntry) {
-  //       const mods = foundEntry.mods;
-  //       const localPrefabs = foundEntry.localPrefabs;
-
-  //       mods.forEach((mod: any) => {
-  //         const fileName = mod.destination.substring(
-  //           mod.destination.lastIndexOf("/") + 1
-  //         );
-  //         const installationFile = new InstallationFile(
-  //           `${host}${mod.source}`,
-  //           `${modsDirectory}/${mod.destination}`,
-  //           fileName
-  //         );
-  //         modFiles.push(installationFile);
-  //       });
-
-  //       localPrefabs.forEach((localPrefab: any) => {
-  //         const fileName = localPrefab.destination.substring(
-  //           localPrefab.destination.lastIndexOf("/") + 1
-  //         );
-  //         const installationFile = new InstallationFile(
-  //           `${host}${localPrefab.source}`,
-  //           `${localPrefabsDirectory}/${localPrefab.destination}`,
-  //           fileName
-  //         );
-  //         modFiles.push(installationFile);
-  //       });
-  //     }
-  //   });
-
-  //   setFilesToDownload(modFiles);
-  //   setFullFileList(modFiles);
-  // };
-
   const onBackClick = (event: any) => {
     router(AppRoutes.citiesAndSettlements);
   };
-
-  // const onDownloadClick = () => {
-  //   // downloadNextFile();
-  //   queueNextFile();
-  // };
-
-  // const onFileDone = (event: any, file: any) => {
-  //   console.log("onFileDone", event, file);
-  //   removeFromInProgressFiles(file);
-  //   addToCompletedFiles(file);
-  //   queueNextFile();
-  // };
-
-  // const onFileError = (event: any, file: any) => {
-  //   console.error("onFileError", event, file);
-  //   removeFromInProgressFiles(file);
-  //   addToErroredFiles(file);
-  //   queueNextFile();
-  // };
-
-  // const queueNextFile = () => {
-  //   const nextFile: any =
-  //     filesToDownload.length > 0 ? filesToDownload[0] : null;
-
-  //   if (nextFile) {
-  //     // removeFromFilesToDownload(nextFile);
-  //     setFilesToDownload((prev: any) =>
-  //       prev.filter((f: any) => f.destination !== nextFile.destination)
-  //     );
-  //     addToQueue(nextFile);
-  //   } else {
-  //     console.warn("Next file not found");
-  //   }
-  // };
-
-  // const addToQueue = (file: any) => {
-  //   setFilesInQueue((prev: any) => [...prev, file]);
-  // };
-
-  // const removeFromQueue = (file: any) => {
-  //   setFilesInQueue((prev: any) =>
-  //     prev.filter((f: any) => f.destination !== file.destination)
-  //   );
-  // };
-
-  // const getOneFromQueue = () => {
-  //   const file = filesInQueue[0];
-  //   removeFromQueue(file);
-  //   return file;
-  // };
-
-  // const addToInProgressFiles = (file: any) => {
-  //   setFilesInProgress((prev: any) => [...prev, file.fileName]);
-  // };
-
-  // const removeFromInProgressFiles = (file: any) => {
-  //   setFilesInProgress((prev: any) => prev.filter((f: any) => f !== file));
-  // };
-
-  // const removeFromFilesToDownload = (file: any) => {
-  //   setFilesToDownload((prev: any) => prev.filter((f: any) => f !== file));
-  // };
 
   const addToCompletedFiles = (file: string) => {
     setFilesCompleted((prev: any) => [...prev, file]);
@@ -295,29 +139,7 @@ export default function Installation() {
 
   return (
     <Box sx={pageContainerStyles}>
-      {/* <Button onClick={ConfigureSelectedFiles}>Test (delete me)</Button> */}
       <Box sx={pageContentStyles}>
-        {/* <div>
-          <Typography>Available Files:</Typography>
-          {availableFiles.map((file: any, index: number) => (
-            <div>{JSON.stringify(file)}</div>
-          ))}
-        </div>
-        <br />
-        <br />
-
-        <div>
-          <Typography>User Selection:</Typography>
-          {step1Selection.map((selection: any, index: number) => (
-            <div>
-              Name: {selection.name}, Children:{" "}
-              {selection.childSelections.length}
-            </div>
-          ))}
-        </div>
-        <br />
-        <br />
-        <br /> */}
         <Typography variant="h1" sx={percentDoneStyles}>
           {downloadProgress.toFixed(1)}%
         </Typography>
@@ -326,14 +148,11 @@ export default function Installation() {
           variant="determinate"
           value={downloadProgress ?? 0}
         ></LinearProgress>
-        {downloadProgress >= 1 && <Typography color="success">Done</Typography>}
-
-        <div>
-          <Typography>Files complete</Typography>
-          {filesCompleted?.length}
-          {JSON.stringify(filesCompleted)}
-        </div>
-        <br />
+        {downloadProgress >= 100 && (
+          <Typography variant="h3" color="success">
+            Done! 😎
+          </Typography>
+        )}
       </Box>
       <Box sx={pageFooterStyles}>
         <Button onClick={onBackClick}>Back</Button>
--- src/Services/Constants.ts
+++ src/Services/Constants.ts
@@ -6,7 +6,6 @@ export enum AppRoutes {
   agreement = "/agreement",
   options = "/options",
   citiesAndSettlements = "/cities-and-settlements",
-  bigStructures = "/big-structures",
   standalonePois = "/standalone-pois",
   installation = "/installation",
   finished = "/finished",
@@ -29,20 +28,16 @@ export const RoutesMeta: Record<AppRoutes, { title: string; value: number }> = {
     title: "Cities and Settlements",
     value: 3,
   },
-  [AppRoutes.bigStructures]: {
-    title: "Big Structures",
-    value: 4,
-  },
   [AppRoutes.standalonePois]: {
     title: "Standalone POIs",
-    value: 5,
+    value: 4,
   },
   [AppRoutes.installation]: {
     title: "Installation",
-    value: 6,
+    value: 5,
   },
   [AppRoutes.finished]: {
     title: "Finished",
-    value: 7,
+    value: 6,
   },
 };
