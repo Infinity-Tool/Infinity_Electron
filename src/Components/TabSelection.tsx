7fcf84c6a0dbc31c6cf16fd33d16dfa78aaf00a5
Alexander Trimble <trimble.a@proton.me>
2023-10-12 18:10:11 -0600

    Started on tab selection component for step 2

--- src/Components/TabSelection.tsx
+++ src/Components/TabSelection.tsx
@@ -4,12 +4,17 @@ import {
   FormControl,
   FormControlLabel,
   Paper,
+  Tab,
+  Tabs,
+  TextField,
   useTheme,
 } from "@mui/material";
 import Loading from "./Loading";
+import { useEffect, useState } from "react";
 
 export default function TabSelection(props: any) {
   const theme = useTheme();
+  const [currentTab, setCurrentTab]: any = useState("All");
 
   const currentSelection = props.currentSelection;
   const availableFiles = props.availableFiles;
@@ -34,74 +39,85 @@ export default function TabSelection(props: any) {
     return childIndex > -1;
   };
 
-  //Styles
-  const modListContainer = {
-    display: "flex",
-    flexDirection: "column",
-    gap: "1rem",
-  };
-  const getModListItemStyles = (file: string) => {
-    const isSelected = getIsSelected(file);
-    return {
-      padding: "1rem",
-      border: isSelected
-        ? `1px solid ${theme.palette.primary.dark}`
-        : "1px solid transparent",
-      // transform: isSelected ? "" : "scale(.96)",
-      // transition: "all .3s ease-in-out",
-    };
-  };
-  const childContainerStyles = {
-    paddingLeft: "2rem",
+  const onTabChange = (event: any, newValue: string) => {
+    setCurrentTab(newValue);
   };
 
   return (
-    <Box sx={modListContainer}>
-      {availableFiles == null && <Loading />}
-      {availableFiles?.map((tas: any, index: number) => (
-        <Paper sx={getModListItemStyles(tas.name)} key={index}>
-          <FormControl>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={getIsSelected(tas.name)}
-                  onClick={(e: any) =>
-                    onParentCheckToggle(e.target.checked, tas.name)
-                  }
-                />
-              }
-              label={tas.name}
-            ></FormControlLabel>
-          </FormControl>
+    <>
+      {/* Search Bar */}
 
-          <Box sx={childContainerStyles}>
-            {tas.childSelections?.length > 0 &&
-              tas.childSelections.map((child: any) => (
-                <Box>
-                  <FormControl>
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          disabled={!getIsSelected(tas.name)}
-                          checked={getIsChildSelected(tas.name, child.name)}
-                          onClick={(e: any) =>
-                            onChildCheckToggle(
-                              e.target.checked,
-                              tas.name,
-                              child.name
-                            )
-                          }
-                        />
-                      }
-                      label={child.name}
-                    ></FormControlLabel>
-                  </FormControl>
-                </Box>
-              ))}
-          </Box>
-        </Paper>
-      ))}
-      {availableFiles == null && <div>loading...</div>}
-    </Box>
+      <Box>
+        <TextField
+          id="outlined-basic"
+          label="Search"
+          variant="outlined"
+          sx={{ width: "100%" }}
+        />
+      </Box>
+
+      {availableFiles == null && <Loading />}
+      <Tabs value={currentTab} onChange={onTabChange}>
+        <Tab label={"All"} value={"All"} />
+
+        {availableFiles?.map((tas: any, index: number) => (
+          <Tab label={tas.name} value={tas.name} />
+        ))}
+      </Tabs>
+
+      {/* <TabSelection value={"All"}>{"ALLLLLLLLL"}</TabSelection>
+      {availableFiles?.map((tas: any, index: number) => (
+        <TabSelection value={tas.name}>{tas.name}</TabSelection>
+      ))} */}
+
+      {/* 
+      <Box sx={modListContainer}>
+        {availableFiles == null && <Loading />}
+        {availableFiles?.map((tas: any, index: number) => (
+          <Paper sx={getModListItemStyles(tas.name)} key={index}>
+            <FormControl>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={getIsSelected(tas.name)}
+                    onClick={(e: any) =>
+                      onParentCheckToggle(e.target.checked, tas.name)
+                    }
+                  />
+                }
+                label={tas.name}
+              ></FormControlLabel>
+            </FormControl>
+
+            <Box sx={childContainerStyles}>
+              {tas.childSelections?.length > 0 &&
+                tas.childSelections.map((child: any) => (
+                  <Box>
+                    <FormControl>
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            disabled={!getIsSelected(tas.name)}
+                            checked={getIsChildSelected(tas.name, child.name)}
+                            onClick={(e: any) =>
+                              onChildCheckToggle(
+                                e.target.checked,
+                                tas.name,
+                                child.name
+                              )
+                            }
+                          />
+                        }
+                        label={child.name}
+                      ></FormControlLabel>
+                    </FormControl>
+                  </Box>
+                ))}
+            </Box>
+          </Paper>
+        ))}
+        {availableFiles == null && <div>loading...</div>}
+      </Box> */}
+    </>
   );
 }
