3d406ce3d286478b5082f99e892fd95739ba4f80
Alexander Trimble <trimble.a@proton.me>
2024-02-12 21:09:23 -0700

    version + publish change test

--- src/renderer/Assets/InfinityLogo.tsx
+++ src/renderer/Assets/InfinityLogo.tsx
@@ -4,8 +4,8 @@ export default function InfinityLogo(props: any) {
   const { devMode } = props;
 
   const theme: any = useTheme();
-  const primary = devMode ? 'cyan' : theme.palette.primary.main;
-  const secondary = devMode ? 'cyan' : theme.palette.secondary.main;
+  const primary = devMode ? 'orange' : theme.palette.primary.main;
+  const secondary = devMode ? 'orange' : theme.palette.secondary.main;
 
   return (
     <svg
